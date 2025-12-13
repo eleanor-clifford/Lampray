e6e9a1d17869aeaf0bbd4a1c0dcc63715c786370
Eleanor Clifford <git@clifford.lol>
2025-12-13 18:54:03 +0000

    Upgrade 'Add mod' button to allow adding multiple mods at a time

--- Lampray/Control/lampControl.h
+++ Lampray/Control/lampControl.h
@@ -193,11 +193,15 @@ namespace Lamp::Core{
                 ImGuiIO &io = ImGui::GetIO();
                 // Add mod button
                 if (ImGui::Button(lampLang::LS("LAMPRAY_ADD_MOD_BUTTON"), ImVec2(io.DisplaySize.x, 0.0f))) {
-                    nfdchar_t *modPath = nullptr;
-                    if (nfdresult_t result = NFD_OpenDialog("zip,rar,7z,tar.xz,tar.gz,.pak", nullptr, &modPath); result == NFD_OKAY) {
-                        puts("Got a file!");
-                        puts(modPath);
-                        FS::lampIO::handleFile(modPath);
+                    nfdpathset_t modPaths;
+                    if (nfdresult_t result = NFD_OpenDialogMultiple("zip,rar,7z,tar.xz,tar.gz,.pak", nullptr, &modPaths); result == NFD_OKAY) {
+                        puts("Got files!");
+                        size_t num_files = NFD_PathSet_GetCount(&modPaths);
+                        for (size_t i = 0; i < num_files; i++) {
+                            printf("Handling file %lu/%lu\n", i+1, num_files);
+                            auto modPath = NFD_PathSet_GetPath(&modPaths, i);
+                            FS::lampIO::handleFile(modPath);
+                        }
                     } else if (result == NFD_CANCEL) {
                         puts("Cancelled!");
                     } else {
