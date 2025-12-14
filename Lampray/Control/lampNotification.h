21810ba57c79bd1d13f767a7d88536599cef0f17
Eleanor Clifford <git@clifford.lol>
2025-12-14 19:00:05 +0000

    Remove function that causes segfault

--- Lampray/Control/lampNotification.h
+++ Lampray/Control/lampNotification.h
@@ -71,14 +71,11 @@ namespace Lamp::Core{
 
                     ImGui::SetWindowFontScale(this->notificationScale);
                     for (auto itt = (*it).begin(); itt != (*it).end(); ++itt) {
-                        std::string wrappedText = (*itt);
+                        std::string text = (*itt);
                         auto spaceAvail = ImGui::GetContentRegionAvail().x;
-                        auto spaceNeeded = ImGui::CalcTextSize(wrappedText.c_str()).x;
+                        auto spaceNeeded = ImGui::CalcTextSize(text.c_str()).x;
 
-                        // attempt to manually wrap the text for long messages
-                        wrappedText = this->manuallyWrapText(wrappedText);
-
-                        if(ImGui::Selectable(wrappedText.c_str())){
+                        if(ImGui::Selectable(text.c_str())){
                             this->clearNotification(outerindex, itt);
                             // go back 1 iteration as we just removed this item. Prevents a crash when clearning a notification.
                             itt--;
@@ -177,53 +174,6 @@ namespace Lamp::Core{
             }
 
             return this->notificationColorValues[notificationType];
-        }
-
-
-        std::string manuallyWrapText(std::string textToWrap){
-            auto spaceAvail = ImGui::GetContentRegionAvail().x;
-            auto spaceNeeded = ImGui::CalcTextSize(textToWrap.c_str()).x;
-            // no wrapping needed
-            if(spaceNeeded < spaceAvail){
-                return textToWrap;
-            }
-
-            std::vector<int> spacesToWrapAt = {};
-            int approxSizePerByte = std::ceil(spaceNeeded / textToWrap.size());
-            int charsThatFit = std::floor(spaceAvail / approxSizePerByte);
-            int prevspace = textToWrap.rfind(" ", charsThatFit);
-
-            // calculate the spots to try to wrap at... NOTE: This does not work if there are not well palced spaces!
-            for(auto i = charsThatFit; i < textToWrap.size(); i+= charsThatFit){
-                prevspace = textToWrap.rfind(" ", i);
-
-                // if we got a repeat prevspace, we are looping the same stuff
-                if(std::find(spacesToWrapAt.begin(), spacesToWrapAt.end(), prevspace) != spacesToWrapAt.end()){
-                    // we are looping, so just add a wrap anyway to continue...
-                    prevspace = i;
-                }
-
-                spacesToWrapAt.push_back(prevspace);
-                i = prevspace;
-            }
-
-            int offsetForInserts = 0;
-            // add newlines to manually wrap the text
-            for(auto it = spacesToWrapAt.begin(); it != spacesToWrapAt.end(); ++it){
-                int strpos = (*it) + offsetForInserts;
-                std::string spaceCheckString;
-                spaceCheckString += textToWrap.at(strpos);
-
-                if(spaceCheckString == " "){
-                    textToWrap.replace(strpos, 1, "\n");
-                } else{
-                    // character was not a space, so insert instead of replace
-                    textToWrap.insert(strpos, "\n");
-                    offsetForInserts += 1;
-                }
-            }
-
-            return textToWrap;
         }
     };
 }
